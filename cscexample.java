2d999be9297cbb0ebde4f8a86cfdbd543a192cea
Panoja123 <Panobaby@gamil.com>
2020-09-21 11:00:15 +0530

    added

--- cscexample.java
+++ cscexample.java
@@ -3,4 +3,34 @@ public class CscExamples
  int questionNo;
  String category;
  String text;
+  public int getQuestionNo()
+ {
+   return questionNo;
+ }
+
+ public String getCategory()
+ {
+   return category;
+ }
+
+ public String getText()
+ {
+   return text;
+ }
+
+ public void setQuestionNo(int a)
+ {
+   questionNo = a;
+ }
+
+ public void setCategory(String b)
+ {
+   category = b;
+ }
+
+ public void setText(String c)
+ {
+   text=c;
+ }
+
 }
