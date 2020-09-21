e8be735111209fe23d03d58a4a3e7d26e35c3a77
Panoja123 <Panobaby@gamil.com>
2020-09-21 11:05:24 +0530

    added

--- cscexample.java
+++ cscexample.java
@@ -3,4 +3,20 @@ public class CscExamples
  int questionNo;
  String category;
  String text;
+ public void addQuestion(CscExamples qu)
+ {
+   setQuestionNo(qu.getQuestionNo());
+   setText(qu.getText());
+   setCategory(qu.getCategory());
+ }
+
+ public void removeQuestion(int quId)
+ {
+
+ }
+
+ public void editQuestion(int quId)
+ {
+
+ }
 }
